cc74b873b1a802c1ad4b3fd23199622e7c5e50a1
Burhan Hayber <Burhan.Hayber@adesso.de>
2019-08-29 18:05:06 +0200

    modify docker-compose file and add new commands to howtostart file

--- src/main/java/de/adesso/testJSF/view/Login.java
+++ src/main/java/de/adesso/testJSF/view/Login.java
@@ -1,7 +1,5 @@
 package de.adesso.testJSF.view;
 
-
-
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
@@ -12,78 +10,72 @@ import javax.faces.event.ActionEvent;
 @SessionScoped
 public class Login implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private String username;
-	private String password;
-	private boolean remember_me = true;
+    private String username;
+    private String password;
+    private boolean remember_me = true;
 
-	public boolean isLoggedIn() {
-		return isLoggedIn;
-	}
+    public boolean isLoggedIn() {
+	return isLoggedIn;
+    }
 
-	public void setLoggedIn(boolean loggedIn) {
-		isLoggedIn = loggedIn;
-	}
+    public void setLoggedIn(boolean loggedIn) {
+	isLoggedIn = loggedIn;
+    }
 
-	public String getAccesToken() {
-		return accesToken;
-	}
+    public String getAccesToken() {
+	return accesToken;
+    }
 
-	public void setAccesToken(String accesToken) {
-		this.accesToken = accesToken;
-	}
+    public void setAccesToken(String accesToken) {
+	this.accesToken = accesToken;
+    }
 
-	private boolean isLoggedIn;
-	private String accesToken;
+    private boolean isLoggedIn;
+    private String accesToken;
 
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+	return password;
+    }
 
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+	this.password = password;
+    }
 
-	/**
-	 * @return the password
-	 */
-	public String getPassword() {
-		return password;
-	}
+    public Login() {
+	System.out.println("Login started!");
+    }
 
-	/**
-	 * @param password
-	 *            the password to set
-	 */
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public String submit(ActionEvent actionEvent) {
+	return "index.xhtml";
+    }
 
-	public Login() {
-		System.out.println("Login started!");
-	}
+    /**
+     * @return the remember_me
+     */
+    public boolean isRemember_me() {
+	return remember_me;
+    }
 
-	public String submit(ActionEvent actionEvent) {
-		// TODO: 19.10.2018 try to obtain access token and set isLoggedIn True
-		// TODO: 19.10.2018 HeaderView Managed Bean erstellen und Requestscoped machen prüfe auf kogged in sonst return login2.xhtml
-		return "index.xhtml";
-	}
+    /**
+     * @param remember_me the remember_me to set
+     */
+    public void setRemember_me(boolean remember_me) {
+	this.remember_me = remember_me;
+    }
 
-	/**
-	 * @return the remember_me
-	 */
-	public boolean isRemember_me() {
-		return remember_me;
-	}
+    public String getUsername() {
+	return username;
+    }
 
-	/**
-	 * @param remember_me
-	 *            the remember_me to set
-	 */
-	public void setRemember_me(boolean remember_me) {
-		this.remember_me = remember_me;
-	}
-
-	public String getUsername() {
-			return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public void setUsername(String username) {
+	this.username = username;
+    }
 }
